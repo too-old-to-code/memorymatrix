8985457e45a79d5f663b1b07266b624680509d38
Andrew Trigg <andrew@Andrews-MacBook-Pro-2.local>
2019-08-31 00:11:21 +0100

    Remove a filesource rule in the config

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,13 +12,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `data`,
         path: `${__dirname}/src/memory-data`,
       },
